b9cb7c746794654ff982be73ad4492f19187790d
ilqara2002 <117916791+ilqara2002@users.noreply.github.com>
2023-01-04 23:18:16 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from "react";
-import { Route, Routes } from "react-router";
-import Navbar from "./components/Navbar";
-import { AuthContextProvider } from "./context/AuthContext";
-import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
-import Account from "./pages/Account";
-import Login from "./pages/Login";
-import MovieList from "./pages/MovieList";
-import ProtectedRoute from "./components/ProtectedRoute";
-import MovieDetails from "./pages/MovieDetails";
+import { ToastContainer } from "react-toastify";
+import AuthContextProvider from "./context/AuthContext";
+import AppRouter from "./router/AppRouter";
 
-function App() {
+const App = () => {
   return (
     <div>
       <AuthContextProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signIn" element={<Login />}></Route>
-          <Route path="/signUp" element={<SignUp />}></Route>
-            <Route path=":genre" element={<MovieList />}></Route>
-            <Route path="/:genre/:movieId" element={<MovieDetails />}></Route>
-          <Route
-            path="/account"
-            element={
-              <ProtectedRoute>
-                <Account />
-              </ProtectedRoute>
-            }
-          ></Route>
-        </Routes>
+        <AppRouter />
+        <ToastContainer />
       </AuthContextProvider>
     </div>
   );
-}
+};
 
 export default App;
